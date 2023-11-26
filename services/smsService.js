92f1ebb10312d8e521aa0a0638a89d90d36e5a68
Aditya Sharma <adityashf7@gmail.com>
2023-11-26 20:38:12 +0530

    updated smsService

--- services/smsService.js
+++ services/smsService.js
@@ -1,14 +1,27 @@
+const twilio = require('twilio');
+require('dotenv').config();
+
 const SmsService = {
-    sendSms: (phoneNumber, message) => {
-        console.log(`Sending SMS to ${phoneNumber}: ${message}`);
+  sendSms: async (phoneNumber, message) => {
+    try {
+      const accountSid = process.env.TWILIO_ACCOUNT_SID;
+      const authToken = process.env.TWILIO_AUTH_TOKEN;
+      const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
-      // const smsGateway = require('your-sms-gateway-package');
-      // smsGateway.send({
-      //   to: phoneNumber,
-      //   body: message,
-      // });
+      const client = twilio(accountSid, authToken);
 
-    },
+      await client.messages.create({
+        body: message,
+        from: twilioPhoneNumber,
+        to: phoneNumber,
+      });
+
+      console.log(`SMS sent to ${phoneNumber}: ${message}`);
+    } catch (error) {
+      console.error('Error sending SMS:', error);
+      throw error;
+    }
+  },
 };
 
 module.exports = SmsService;
